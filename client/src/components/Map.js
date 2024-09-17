86f8202aa134f8e619209365d60fda9699d08801
Ethan Key <ethanrkey23@gmail.com>
2024-09-17 13:23:21 -0400

    added color coded pins to mark location of conflict by type

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,7 +1,38 @@
-
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Define custom icons for different conflict types using external PNGs
+const militaryIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  shadowSize: [41, 41],
+  shadowAnchor: [12, 41],
+});
+
+const economicIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  shadowSize: [41, 41],
+  shadowAnchor: [12, 41],
+});
+
+const geopoliticalIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-yellow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  shadowSize: [41, 41],
+  shadowAnchor: [12, 41],
+});
 
 const Map = ({ data, filters }) => {
   // Filter data based on the year and type
@@ -11,6 +42,20 @@ const Map = ({ data, filters }) => {
     return inYearRange && matchesType;
   });
 
+  // Function to get icon based on conflict type
+  const getIconByType = (type) => {
+    switch (type) {
+      case 'military':
+        return militaryIcon;
+      case 'economic':
+        return economicIcon;
+      case 'geopolitical':
+        return geopoliticalIcon;
+      default:
+        return militaryIcon; // Default to military if type is not specified
+    }
+  };
+
   return (
     <div className="map-container">
       <MapContainer center={[20, 0]} zoom={2} className="map-container">
@@ -19,7 +64,11 @@ const Map = ({ data, filters }) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         {filteredData.map((conflict) => (
-          <Marker key={conflict.id} position={[conflict.lat, conflict.lon]}>
+          <Marker
+            key={conflict.id}
+            position={[conflict.lat, conflict.lon]}
+            icon={getIconByType(conflict.type)}
+          >
             <Popup>
               <strong>{conflict.name}</strong>
               <br />
